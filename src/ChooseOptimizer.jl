55c424e87c282bfb103c3caa0311e5b970dd8127
Ed Scheinerman <ers@jhu.edu>
2019-11-09 11:55:00 -0500

    First beta ready

--- src/ChooseOptimizer.jl
+++ src/ChooseOptimizer.jl
@@ -1,22 +1,24 @@
 module ChooseOptimizer
 
-using Cbc
+using Cbc, JuMP
 
 export set_optimizer, get_optimizer
 
 SOLVER = Cbc
 
-function set_optimizer(OPT:_NAME:Module=Cbc)
+function set_optimizer(OPT_NAME::Module=Cbc)
     global SOLVER = OPT_NAME
 end
+
+function get_optimizer()
+    global SOLVER
+    return JuMP.with_optimizer(SOLVER.Optimizer)
+end
+
 
 # function set_optimizer_options(opts)
 # function get_optimizer_options()
 
-function get_optimizer()
-    global SOLVER
-    return SOLVER.Optimizer()
-end
 
 
 end
