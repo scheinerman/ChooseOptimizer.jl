2efd83333c97270cffc0f030f3dc5ff72c884866
Ed Scheinerman <ers@jhu.edu>
2022-04-18 20:53:46 -0400

    improved set_solver_verbose

--- src/ChooseOptimizer.jl
+++ src/ChooseOptimizer.jl
@@ -73,86 +73,8 @@ function get_solver_name()
     return _SOLVER
 end
 
-"""
-`set_solver_verbose(verb = true)` sets how verbose the solver is
-during its work.
-"""
-function set_solver_verbose(verb::Bool = true)
-    global _SOLVER
-    val = verb ? 1 : 0
-    key = "Unknown"
-    # @info "Setting verbose option for $_SOLVER to $verb"
-    try
-        if _SOLVER == Cbc
-            key = "LogLevel"
-        end
-    catch
-    end
 
-    try
-        if _SOLVER == Main.Cbc
-            key = "LogLevel"
-        end
-    catch
-    end
-
-    try
-        if _SOLVER == Gurobi
-            key = "OutputFlag"
-        end
-    catch
-    end
-
-    try
-        if _SOLVER == Main.Gurobi
-            key = "OutputFlag"
-        end
-    catch
-    end
-
-    try
-        if _SOLVER == GLPK
-            key = "msg_lev"
-            val = verb ? 2 : 0 # GLPK uses 2 for "normal" messaging
-        end
-    catch
-    end
-
-    try
-        if _SOLVER == Main.GLPK
-            key = "msg_lev"
-            val = verb ? 2 : 0 # GLPK uses 2 for "normal" messaging
-        end
-    catch
-    end
-
-    try
-        if _SOLVER == HiGHS
-            key = "output_flag"
-            val = verb
-        end
-    catch
-    end
-
-    try
-        if _SOLVER == Main.HiGHS
-            key = "output_flag"
-            val = verb
-        end
-    catch
-    end
-
-
-
-    if key == "Unknown"
-        @warn("Unable to set verbose option for $_SOLVER")
-    else
-        set_solver_options(key, val)
-    end
-    nothing
-end
-
-
+include("set_verbose.jl")
 
 
 end
---
+++ src/set_verbose.jl
@@ -0,0 +1,31 @@
+"""
+The `_solver_table` maps solver Modules as Symbols (e.g., `:Cbc`) to 
+a pair `(name, verbose, quite)` values.
+"""
+_solver_table = Dict{String,Tuple{String,Any,Any}}()
+
+_solver_table["Cbc"] = ("LogLevel", 1, 0)
+_solver_table["Gurobi"] = ("OutputFlag", 1, 0)
+_solver_table["GLPK"] = ("msg_lev", 2, 0)
+_solver_table["HiGHS"] = ("output_flag", true, false)
+
+
+"""
+`set_solver_verbose(verb = true)` sets how verbose the solver is
+during its work.
+"""
+function set_solver_verbose(verb::Bool = true)
+
+    the_solver = string(_SOLVER)
+
+    if !haskey(_solver_table, the_solver)
+        @warn("Unable to set verbose option for $the_solver")
+        return
+    end
+
+    (opt, verbose, quiet) = _solver_table[the_solver]
+
+    set_solver_options(opt, verb ? verbose : quiet)
+    @info "Solver $_SOLVER verbose is set to $verb"
+end
+
